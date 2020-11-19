56fecdef9b764d4039b81cb20132f107b540ac5c
jonnyhak <jonnyhak@gmail.com>
2020-11-19 18:53:41 -0500

    commit tree 93700abd0b2b2ad737e44c906f56fc5151539d58

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// record.find( function(e) { return e.result === "W"} ) 
+
+
+superbowlWin = (record) => {
+  let outcome = record.find( record => record.result === "W")
+  if (outcome) {
+    return outcome.year
+  } else {
+    undefined 
+  }
+}
